155228e8895f543f07b6ec9bac1f89a24d9a5644
pollythepocket <pollyruhnke@gmail.com>
2025-03-30 13:30:25 -0500

    more inital stuff -- ugly

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
+//reroutes default ad websites
 browser.webRequest.onBeforeRequest.addListener(
     function(details) {
-      // Block requests to ad-related URLs
       if (details.url.includes("doubleclick.net") || details.url.includes("googleads.g.doubleclick.net")) {
         return {cancel: true};
       }
